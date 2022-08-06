978c5605aee90f70c8077350d5c2424d29873331
freerpolymer <freerpolymer@126.com>
2022-08-06 18:57:41 +0800

    项目结构更新，设计文档更新

---
+++ DBPROVIDER/src/main/java/com/polymer/provider/Main.java
@@ -0,0 +1,7 @@
+package com.polymer.provider;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
---
+++ SERVERAPI/src/main/java/com/polymer/api/Main.java
@@ -0,0 +1,7 @@
+package com.polymer.api;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
